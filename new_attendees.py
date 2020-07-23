be4c6227e3844eb6183b43962a9bd940d38477a4
Jack Parsons <jcksnparsons@yahoo.com>
2020-07-23 10:18:59 -0500

    used a larger data set and tried out memoizing the data per suggestion. definitely increased performance by a ton

--- new_attendees.py
+++ new_attendees.py
@@ -1,11 +1,25 @@
 import csv
 import sqlite3
+import timeit
 
 """
     Messing around with manipulating CSV files in python.
 """
 
 connection = "/Users/jcksnparsons/coding-practice/csv_manipulation/db.sqlite3"
+
+
+def memoize(etl_func):
+    cache = dict()
+
+    def memoized_func(*args):
+        if args in cache:
+            return cache
+        result = etl_func(*args)
+        cache[args] = result
+        return result
+
+    return memoized_func
 
 
 def post_to_db(my_list):
@@ -18,10 +32,11 @@ def post_to_db(my_list):
                 VALUES (?, ?, ?)""",
                               (person['first_name'], person['last_name'], person['email']))
 
-
+@memoize
 def get_emails_from_file(file_string):
     """
-        Takes in a csv file and returns a list of dictionaries, each containing the contact information for a party attendee.
+        Takes in csv filename, which is parsed into a list of dictionaries. The function then uses the function above to iterate over that
+        list and export each dict into a sql database.
     """
 
     with open(file_string) as csv_file:
@@ -38,7 +53,7 @@ def get_emails_from_file(file_string):
 
             list_of_attendees.append(person_dict)
 
-        post_to_db(list_of_attendees)
+        return list_of_attendees
 
+print(timeit.timeit("get_emails_from_file('MOCK_DATA.csv')", globals=globals(), number=5000))
 
-get_emails_from_file('attendees1.csv')
